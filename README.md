0703f2bbdf0672e0769ac0c52d597e3f523120a1
Sebastien Renaut <sebastien.renaut@gmail.com>
2025-04-14 13:03:44 -0400

    1st commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # LMNA
+  * Genotyping analyses (plink, vcftools, plots)
+
+
+
+# Further information
+  * sebastien.renaut.1@ulaval.ca
---
+++ scripts/plink_phasing.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+#original data is here:
+#mkdir LMNA_148_samples
+#cp /home/renseb01/Documents/mendel_Christian_Steinberg/YB-P01_LORD_only_Plus_Steinberg_LMNA_148_samples/PLINK_200225_0230/* LMNA_148_samples/.
+
+#sort and  make pgen
+/home/renseb01/plink2 --ped YB-P01_LORD_only_Plus_Steinberg_LMNA_148_samples.ped --map YB-P01_LORD_only_Plus_Steinberg_LMNA_148_samples.map --make-pgen --out YB-P01_LORD_only_Plus_Steinberg_LMNA_148_samples --allow-extra-chr --sort-vars
+
+#make the fam file
+/home/renseb01/plink2 --pfile YB-P01_LORD_only_Plus_Steinberg_LMNA_148_samples --make-bed --out YB-P01_LORD_only_Plus_Steinberg_LMNA_148_samples --allow-extra-chr
+
+#make a .vcf file
+/home/renseb01/plink2 --bfile YB-P01_LORD_only_Plus_Steinberg_LMNA_148_samples --recode vcf --out YB-P01_LORD_only_Plus_Steinberg_LMNA_148_samples
+
+#remove duplicates, keep only chromosome1, and filter unusable regions.
+#filter MAF <0.05
+vcftools --vcf YB-P01_LORD_only_Plus_Steinberg_LMNA_148_samples.vcf --maf 0.05 --out YB-P01_LORD_only_Plus_Steinberg_LMNA_148_samples_MAF005 --recode
+
+head -33 YB-P01_LORD_only_Plus_Steinberg_LMNA_148_samples_MAF005.recode.vcf >header
+
+awk '!($1 == 1 && $2 < 2160299)' YB-P01_LORD_only_Plus_Steinberg_LMNA_148_samples_MAF005.recode.vcf >temp.vcf
+awk '$1 != "X"' temp.vcf >temp.vcf2
+awk '$1 != "XY"' temp.vcf2 >temp.vcf3
+awk '$1 != "Y"' temp.vcf3 >temp.vcf4
+awk '$1 != "MT"' temp.vcf4 >temp.vcf5
+awk '!seen[$1, $2]++' temp.vcf5 >temp.vcf6
+awk '$4 != "D"' temp.vcf6 >temp.vcf7
+awk '$4 != "I"' temp.vcf7 >temp.vcf8
+awk '$4 != "N"' temp.vcf8 >temp.vcf9
+grep -iv 'ilmnseq' temp.vcf9 >temp.vcf10
+grep -iv 'Dup' temp.vcf10 >temp.vcf11
+cat temp.vcf11 >>header
+mv header filtered.vcf
+
+#phasing
+beagle gt=filtered.vcf out=chr_phased
+
+#reformat for chr1
+zcat chr_phased.vcf.gz | head -9 >chr1_phased.vcf
+zcat chr_phased.vcf.gz | awk '$1 == "1"' >>chr1_phased.vcf
+gzip chr1_phased.vcf
+
+#cleanup
+rm temp*
+rm header
+
+
+
+####THE REST (below) is DONE via an Rscript locally
+
+
+#####CLEANUP THE PHASED .vcf
+mkdir LMNA_52_phased
+
+
+
+
+########HAPLOBLOCKS
+#make bed file
+/home/renseb01/plink2 --vcf chr1_h12.vcf --make-bed --out chr1
+
+#find haplo blocks
+plink --blocks-strong-lowci 0.6 --blocks-strong-highci 0.84 --blocks-recomb-highci 0.8 --bfile chr1 --blo
+cks no-pheno-req --out chr1_haploblocks
