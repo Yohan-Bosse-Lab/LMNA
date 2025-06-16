719c7bfd93626ea2ab974d53e78287a0c40ec0b5
Sebastien Renaut <sebastien.renaut@gmail.com>
2025-06-16 12:23:20 -0400

    udpate

--- bash/plink_gwas.sh
+++ bash/plink_gwas.sh
@@ -1,4 +1,3 @@
-#cd
 cd data
 
 #calculate Principal Components based on genotypic data
@@ -10,23 +9,15 @@ cd data
 #Sort the output from above based on the alphabetic sample IDs.
 (head -n 1 LMNA_52_phased/pca_loadings.eigenvec && tail -n +2 LMNA_52_phased/pca_loadings.eigenvec | sort -k1,1) >LMNA_52_phased/covariate.tsv
 
-#add it to the covariates (severe.txt in prepared in GWAS.Rmd).
+#add it to the covariates (severe.txt in prepared in GWAS.Rmd) file.
 paste covariate.tsv severe.tsv >.LMNA_52_phased/phenotype.txt
-
-#NO COVARIATES Do the glm in plink
-#/home/renseb01/plink2 \
-#  --vcf ../LMNA_52_phased/chr_52.vcf \
-#  --pheno ../LMNA_52_phased/severe.tsv \
-#  --pheno-name severity_recoded \
-#  --glm allow-no-covars \
-#  --out ../LMNA_52_phased/gwas_results
 
 #glm with covariates
 /home/renseb01/plink2 \
   --vcf ../LMNA_52_phased/chr_52.vcf \
   --pheno ../LMNA_52_phased/phenotype.txt \
   --pheno-name severity_recoded \
-  --covar-name sex_recoded PC1 PC2 PC3 \
+  --covar-name sex_recoded PC1 PC2 \
   --glm \
   --out ../LMNA_52_phased/gwas_results
 
