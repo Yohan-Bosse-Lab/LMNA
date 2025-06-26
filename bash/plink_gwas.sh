60b6091c547ceba3190635e2935fc2801d936a79
Sebastien Renaut <sebastien.renaut@gmail.com>
2025-06-26 10:22:27 -0400

    updates

--- bash/plink_gwas.sh
+++ bash/plink_gwas.sh
@@ -1,4 +1,4 @@
-cd data
+cd ../data
 
 #calculate Principal Components based on genotypic data
 /home/renseb01/plink2 \
@@ -10,14 +10,14 @@ cd data
 (head -n 1 LMNA_52_phased/pca_loadings.eigenvec && tail -n +2 LMNA_52_phased/pca_loadings.eigenvec | sort -k1,1) >LMNA_52_phased/covariate.tsv
 
 #add it to the covariates (severe.txt in prepared in GWAS.Rmd) file.
-paste covariate.tsv severe.tsv >.LMNA_52_phased/phenotype.txt
+paste LMNA_52_phased/covariate.tsv clinical/severe.tsv >LMNA_52_phased/phenotype.txt
 
 #glm with covariates
 /home/renseb01/plink2 \
-  --vcf ../LMNA_52_phased/chr_52.vcf \
-  --pheno ../LMNA_52_phased/phenotype.txt \
+  --vcf LMNA_52_phased/chr_52.vcf \
+  --pheno LMNA_52_phased/phenotype.txt \
   --pheno-name severity_recoded \
   --covar-name sex_recoded PC1 PC2 \
   --glm \
-  --out ../LMNA_52_phased/gwas_results
+  --out LMNA_52_phased/gwas_results
 
